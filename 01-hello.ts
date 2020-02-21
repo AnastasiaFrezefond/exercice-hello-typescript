3bf86aff6f2dbe00d56fc1e8648d44ec009c7fc9
Anastasia Frezefond <anastasia_frezefond@hotmail.com>
2020-02-21 10:28:02 +0100

    first commit

---
+++ 01-hello.ts
@@ -0,0 +1,10 @@
+// Hour 1 - TypeScript
+
+const texte = "Hello World !";
+console.log(texte);
+
+// Tout ce qu'on a appris en JS on peut l'écrire dans un fichier.ts
+// Ce fichier n'est pas compris par les navigateur web 
+// Du coup il faut le transpiler en JS qui sera lui même compilé 
+// Pour se faire, on va utiliser notre terminal tsc *.ts
+
